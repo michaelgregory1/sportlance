78caf2008b0a5a106749263a157b091cdcc41b89
Nicolas Hennick <nicolashennick@gmail.com>
2018-11-20 11:42:32 +0000

    Added validation for bookings and locations models. Generated routes and controller for bookings and locations.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,6 @@
 class Location < ApplicationRecord
   belongs_to :user
+  has_many :bookings
+  validates :longitude, presence: true, numericality: { only_float: true }
+  validates :latitude, presence: true, numericality: { only_float: true }
 end
