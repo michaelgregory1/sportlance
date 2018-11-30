ac640cff613879cda7856d435c1a2848190875c0
Ariel LeRoy <arielmatsuileroy@gmail.com>
2018-11-30 09:40:08 +0000

    star rating system improved

--- app/javascript/packs/review.js
+++ app/javascript/packs/review.js
@@ -3,6 +3,7 @@ const stars = document.querySelectorAll('.star')
 const addStar = (element) => {
 	element.classList.add("glyphicon-star");
 	element.classList.remove("glyphicon-star-empty");
+	setRating(element);
 	const next = element.previousElementSibling;
 	if (next.classList.contains('glyphicon-star-empty')) {
 		addStar(next);
@@ -13,8 +14,14 @@ const removeStar = (element) => {
 	if (element.nextElementSibling.classList.contains("glyphicon-star")) {
 		element.nextElementSibling.classList.remove("glyphicon-star");
 		element.nextElementSibling.classList.add("glyphicon-star-empty");
+		setRating(element);
 	};
 	removeStar(element.nextElementSibling);
+}
+
+const setRating = (element) => {
+	let selector = document.getElementById("star-selector");
+	selector.value = document.querySelectorAll('.glyphicon-star').length;
 }
 
 if (stars) {
